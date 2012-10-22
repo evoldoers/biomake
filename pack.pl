b027a60a74f1072b9f05735481d61da49f8359dd
Chris Mungall <cjm@berkeleybop.org>
2012-10-21 17:11:47 -0700

    prepare for release

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(plmake).
 title('Prolog makefile-like system').
-version('0.0.2').
+version('0.0.3').
 author('Chris Mungall','cmungall@gmail.com').
 home('https://github.com/cmungall/plmake').
 download( 'http://www.berkeleybop.org/~cjm/packs/plmake-*.tgz' ).
