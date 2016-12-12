f5377d7dfaf355d4c3b361cf3f65b4950eb1b938
Chris Mungall <cjm@berkeleybop.org>
2016-12-11 19:12:45 -0800

    using github for releases

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 name(biomake).
 title('Prolog makefile-like system').
-version('0.0.3').
-download( 'http://www.berkeleybop.org/~cjm/packs/biomake-*.tgz' ).
+version('0.1.0').
+download( 'https://github.com/evoldoers/biomake/releases/*.zip' ).
 author('Chris Mungall','cmungall@gmail.com').
 author('Ian Holmes','ihh@berkeley.edu').
 packager('Chris Mungall','cmungall@gmail.com').
-home('https://github.com/cmungall/biomake').
+home('https://github.com/evoldoers/biomake').
